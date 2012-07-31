f8d6edaef0735768792f2060fd8b4e00ff6e04ce
Aditya Vaidya <kroq.gar78@gmail.com>
2012-07-31 00:12:02 -0500

    Imported debian/ from asm3 3.3.2-1

---
+++ debian/orig-tar.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+set -eu
+
+VERSION=$2
+TAR=../asm3_$VERSION.orig.tar.gz
+DIR=asm-$VERSION
+TAG=$(echo "ASM_$VERSION" | sed -re's,\.,_,g')
+
+svn export svn://svn.forge.objectweb.org/svnroot/asm/tags/${TAG}/ $DIR
+tar -c -z -f $TAR --exclude '*.jar' $DIR
+rm -rf $DIR ../listing.php
+
+# move to directory 'tarballs'
+if [ -r .svn/deb-layout ]; then
+  . .svn/deb-layout
+  mv $TAR $origDir && echo "moved $TAR to $origDir"
+fi
+
